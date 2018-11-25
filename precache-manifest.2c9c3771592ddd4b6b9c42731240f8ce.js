b75d676359d037da7a8aa980cc1bf8b48bbe4d09
Yonny Leandro Sánchez Beltrán <yonny.sanchez@globant.com>
2018-11-25 14:11:33 -0500

    First static pokedex

---
+++ precache-manifest.2c9c3771592ddd4b6b9c42731240f8ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6dc0eccbafde13f2dba2",
+    "url": "/static/js/main.6dc0eccb.chunk.js"
+  },
+  {
+    "revision": "76f2ead83c0fb759b9e1",
+    "url": "/static/js/1.76f2ead8.chunk.js"
+  },
+  {
+    "revision": "6dc0eccbafde13f2dba2",
+    "url": "/static/css/main.b48a7b0d.chunk.css"
+  },
+  {
+    "revision": "51f5891d1ae6a794939a884ace41bbf9",
+    "url": "/index.html"
+  }
+];
